0c8ef2db6e850622233a2b2fabe18aefc4edcf3d
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-03-29 00:58:41 +0530

    Initial commit

---
+++ app/src/main/java/com/example/foodforgood/Splash_Screen.kt
@@ -0,0 +1,22 @@
+package com.example.foodforgood
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+@Suppress("DEPRECATION")
+class Splash_Screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
