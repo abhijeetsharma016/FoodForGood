5e9b33809c3d47f84843a7ae1f0cdf63dccc245f
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-03-30 21:06:56 +0530

    added navigation to login screen and signup screen

--- app/src/main/java/com/example/foodforgood/SignupActivity.kt
+++ app/src/main/java/com/example/foodforgood/SignupActivity.kt
@@ -1,20 +1,24 @@
 package com.example.foodforgood
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.foodforgood.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
+    private val binding: ActivitySignupBinding by lazy {
+        ActivitySignupBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_signup)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+        binding.alreadyhaveaccountbutton.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
+
     }
 }
