53b0df3bcf93f07256a71ab400c7faa1fb411f90
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-04-02 20:20:31 +0530

    initilised popular Adapter

---
+++ app/src/main/java/com/example/foodforgood/adapter/popularAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.foodforgood.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Recycler
+import com.example.foodforgood.databinding.FragmentHomeBinding
+import com.example.foodforgood.databinding.PopularItemBinding
+
+class popularAdapter: RecyclerView.Adapter<popularAdapter.PopularViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PopularViewHolder {
+        TODO("Not yet implemented")
+    }
+
+
+    override fun onBindViewHolder(holder: PopularViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class PopularViewHolder(private val binding: PopularItemBinding):RecyclerView.ViewHolder(binding.root) {
+
+    }
+}
