ce360c3342ba4dfb7a7652add74e4f21cd416acb
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-04-03 09:47:51 +0530

    Set up the adapter in home view

--- app/src/main/java/com/example/foodforgood/Fragment/HomeFragment.kt
+++ app/src/main/java/com/example/foodforgood/Fragment/HomeFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.denzcoskun.imageslider.constants.ScaleTypes
 import com.denzcoskun.imageslider.interfaces.ItemClickListener
 import com.denzcoskun.imageslider.models.SlideModel
 import com.example.foodforgood.R
+import com.example.foodforgood.adapter.popularAdapter
 import com.example.foodforgood.databinding.FragmentHomeBinding
 
 
@@ -55,6 +58,14 @@ class HomeFragment : Fragment() {
                 Toast.makeText(requireContext(), itemMessage, Toast.LENGTH_SHORT).show()
             }
         })
+
+        val foodName = listOf("Burger", "Sandwich", "momo", "item")
+        val Price = listOf("$5", "$7", "$8", "$10")
+        val popularFoodImages = listOf(R.drawable.menu1, R.drawable.menu2, R.drawable.menu3, R.drawable.menu5)
+
+        val adapter = popularAdapter(foodName, Price,popularFoodImages)
+        binding.PopularRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.PopularRecyclerView.adapter = adapter
     }
 
     companion object {
