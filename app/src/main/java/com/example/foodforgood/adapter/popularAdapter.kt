ec32ca4deb62381c81b07dee5d6f6005079c329c
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-04-03 09:35:01 +0530

    made adapter

--- app/src/main/java/com/example/foodforgood/adapter/popularAdapter.kt
+++ app/src/main/java/com/example/foodforgood/adapter/popularAdapter.kt
@@ -1,28 +1,38 @@
 package com.example.foodforgood.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Recycler
 import com.example.foodforgood.databinding.FragmentHomeBinding
 import com.example.foodforgood.databinding.PopularItemBinding
 
-class popularAdapter: RecyclerView.Adapter<popularAdapter.PopularViewHolder>() {
+class popularAdapter(private var items:List<String>,private val price:List<String>, private val image: List<Int>): RecyclerView.Adapter<popularAdapter.PopularViewHolder>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PopularViewHolder {
-        TODO("Not yet implemented")
+        return PopularViewHolder(PopularItemBinding.inflate(LayoutInflater.from(parent.context), parent, false ))
     }
 
 
     override fun onBindViewHolder(holder: PopularViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item = items[position]
+        val images = image[position]
+        val price = price[position]
+        holder.bind(item, price, images)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return items.size
     }
 
     class PopularViewHolder(private val binding: PopularItemBinding):RecyclerView.ViewHolder(binding.root) {
+        private val imageView = binding.imageView6
+        fun bind(item: String,price: String, images: Int) {
+            binding.foodNamePopular.text = item
+            binding.pricePopular.text = price
+            imageView.setImageResource(images)
+        }
 
     }
 }
