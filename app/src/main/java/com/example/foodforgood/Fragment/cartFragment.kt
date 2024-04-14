70983f10c41f895688c7e23dc496b6d96abcd976
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-04-15 00:00:47 +0530

    cart view implemented for dummy data

--- app/src/main/java/com/example/foodforgood/Fragment/cartFragment.kt
+++ app/src/main/java/com/example/foodforgood/Fragment/cartFragment.kt
@@ -5,11 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.foodforgood.R
+import com.example.foodforgood.adapter.cartAdapter
+import com.example.foodforgood.databinding.CartItemBinding
+import com.example.foodforgood.databinding.FragmentCartBinding
 
 
 class cartFragment : Fragment() {
-
+    private lateinit var binding: FragmentCartBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,8 +24,23 @@ class cartFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cart, container, false)
+        binding = FragmentCartBinding.inflate(inflater, container, false)
+
+        val cartFoodName = listOf("Burger", "sandwich", "momo", "item", "sandwich", "momo")
+        val cartItemPrice = listOf("$5", "$6", "$8", "$9", "$10", "$11")
+        val cartImage = listOf(
+            R.drawable.menu1,
+            R.drawable.menu2,
+            R.drawable.menu3,
+            R.drawable.menu4,
+            R.drawable.menu5,
+            R.drawable.menu2
+        )
+
+        val adapter = cartAdapter(ArrayList(cartFoodName), ArrayList(cartItemPrice), ArrayList(cartImage))
+        binding.cartRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.cartRecyclerView.adapter = adapter
+        return binding.root
     }
 
     companion object {
