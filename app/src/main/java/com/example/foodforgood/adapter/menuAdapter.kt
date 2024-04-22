4ce1327a2eaa2707dcf929ae09bbfaec14d7d97b
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-04-22 23:35:21 +0530

    Made menu Adapter

---
+++ app/src/main/java/com/example/foodforgood/adapter/menuAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.foodforgood.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.foodforgood.databinding.FragmentMenuBottomSheetBinding
+import com.example.foodforgood.databinding.MenuItemBinding
+
+class menuAdapter(private val menuItemName: MutableList<String>, private val menuItemPrice : MutableList<String>, private val MenuImage:MutableList<Int>): RecyclerView.Adapter<menuAdapter.MenuViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MenuViewHolder {
+        val binding = MenuItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MenuViewHolder(binding)
+    }
+
+
+    override fun onBindViewHolder(holder: MenuViewHolder, position: Int) {
+        holder.bind(position)
+    }
+    override fun getItemCount(): Int = menuItemName.size
+
+    inner class MenuViewHolder(private val binding: MenuItemBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(position: Int) {
+            binding.apply {
+                menuFoodName.text = menuItemName[position]
+                menuPrice.text = menuItemPrice[position]
+                menuImage.setImageResource(MenuImage[position])
+            }
+        }
+
+    }
+}
