f224a3f4443379000723409bd1166d014c635d38
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-05-30 23:58:36 +0530

    Notification Adapter

---
+++ app/src/main/java/com/example/foodforgood/adapter/NotificationAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.foodforgood.adapter
+
+import android.app.Notification
+import android.view.ViewGroup
+import android.view.LayoutInflater
+import androidx.recyclerview.widget.RecyclerView
+import com.example.foodforgood.databinding.NotificationItemBinding
+
+class NotificationAdapter(private val notification: ArrayList<String>, private val notificationImage: ArrayList<Int>): RecyclerView.Adapter<NotificationAdapter.NotificationViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotificationViewHolder {
+        val binding = NotificationItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return NotificationViewHolder(binding)
+    }
+
+
+    override fun onBindViewHolder(holder: NotificationViewHolder, position: Int) {
+        holder.bind(position)
+    }
+
+    override fun getItemCount(): Int = notification.size
+
+
+    inner class NotificationViewHolder(private val binding: NotificationItemBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(position: Int) {
+            TODO("Not yet implemented")
+        }
+
+    }
+
+}
