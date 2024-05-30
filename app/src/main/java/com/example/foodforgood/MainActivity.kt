f9a5c556d212957541a5824993ae6024dec1405b
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-05-31 00:59:17 +0530

    linked notfication with main activity

--- app/src/main/java/com/example/foodforgood/MainActivity.kt
+++ app/src/main/java/com/example/foodforgood/MainActivity.kt
@@ -5,14 +5,23 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.foodforgood.adapter.NotificationAdapter
+import com.example.foodforgood.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         var NavController = findNavController(R.id.fragmentContainerView2)
         var bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         bottomNav.setupWithNavController(NavController)
+        binding.notificationButton.setOnClickListener{
+            val bottomSheetDialog = notification_bottom_fragment()
+            bottomSheetDialog.show(supportFragmentManager, "test")
+        }
+
     }
 }
