e955410a89db600fe0f1342ac83f7494397458be
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-06-06 02:31:53 +0530

    payout activity

---
+++ app/src/main/res/drawable/foodforgood/PayOutActivity.kt
@@ -0,0 +1,21 @@
+package drawable.foodforgood
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.foodforgood.R
+
+class PayOutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_pay_out)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
