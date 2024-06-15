7003d94d621dca1eaf000fbbd8c2735cd85c9361
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-06-16 02:07:08 +0530

    signup activity backend done

--- AdminFoodForGood/app/src/main/java/com/example/adminfoodforgood/LoginActivity.kt
+++ AdminFoodForGood/app/src/main/java/com/example/adminfoodforgood/LoginActivity.kt
@@ -1,20 +1,30 @@
 package com.example.adminfoodforgood
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.adminfoodforgood.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private val binding: ActivityLoginBinding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+        binding.LoginButton.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        binding.dontHaveAnAccountButton.setOnClickListener{
+            val intent = Intent(this, signupActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
--- AdminFoodForGood/app/src/main/java/com/example/adminfoodforgood/signupActivity.kt
+++ AdminFoodForGood/app/src/main/java/com/example/adminfoodforgood/signupActivity.kt
@@ -1,20 +1,44 @@
 package com.example.adminfoodforgood
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.adminfoodforgood.databinding.ActivitySignupBinding
 
 class signupActivity : AppCompatActivity() {
+    private val binding: ActivitySignupBinding by lazy { ActivitySignupBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_signup)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+        binding.CreateAccount.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
         }
+        binding.alreadyHaveAnAccountButton.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        val locationList = arrayOf(
+            "Jaipur",
+            "Agra",
+            "Delhi",
+            "Mumbai",
+            "Pune",
+            "Chennai",
+            "Kolkata",
+            "Hyderabad",
+            "Ahmedabad",
+            "Surat"
+        )
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, locationList)
+        val autoCompleteTextView = binding.listOfLocations
+        autoCompleteTextView.setAdapter(adapter)
     }
 }
