5eb4cf2d0159de9d0ff7ac7108282b989276d4d5
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-06-18 01:58:06 +0530

    Admin add menu done

--- AdminFoodForGood/app/src/main/java/com/example/adminfoodforgood/addItemActivity.kt
+++ AdminFoodForGood/app/src/main/java/com/example/adminfoodforgood/addItemActivity.kt
@@ -2,19 +2,35 @@ package com.example.adminfoodforgood
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.adminfoodforgood.databinding.ActivityAddItemBinding
 
 class addItemActivity : AppCompatActivity() {
+    private val binding: ActivityAddItemBinding by lazy {
+        ActivityAddItemBinding.inflate(
+            layoutInflater
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_add_item)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+        binding.selectImage.setOnClickListener {
+            pickImage.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
+        binding.backButton.setOnClickListener {
+            finish()
+        }
+    }
+
+    val pickImage = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+        if (uri != null) {
+            binding.selectedImage.setImageURI(uri)
         }
     }
 }
