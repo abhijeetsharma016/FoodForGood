85a012801f898e9454cff2efeab9552e72ee69c5
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-06-21 00:45:00 +0530

    profile done

--- AdminFoodForGood/app/src/main/java/com/example/adminfoodforgood/adminProfileActivity.kt
+++ AdminFoodForGood/app/src/main/java/com/example/adminfoodforgood/adminProfileActivity.kt
@@ -11,8 +11,28 @@ class adminProfileActivity : AppCompatActivity() {
     private val binding: ActivityAdminProfileBinding by lazy { ActivityAdminProfileBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(binding.root)
+        binding.backButton.setOnClickListener{
+            finish()
+        }
+        binding.name.isEnabled = false
+        binding.address.isEnabled = false
+        binding.phone.isEnabled = false
+        binding.email.isEnabled = false
+        binding.password.isEnabled = false
 
+        var isEnable = false
+        binding.editButton.setOnClickListener {
+            isEnable = !isEnable
+            binding.name.isEnabled = isEnable
+            binding.address.isEnabled = isEnable
+            binding.phone.isEnabled = isEnable
+            binding.email.isEnabled = isEnable
+            binding.password.isEnabled = isEnable
+
+            if (isEnable) {
+                binding.name.requestFocus()
+            }
+        }
     }
 }
