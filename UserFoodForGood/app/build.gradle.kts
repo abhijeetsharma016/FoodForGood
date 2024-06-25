39e9b66b1fbb931a2357a05132ac59d9688ce240
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-06-26 02:21:25 +0530

    google button signin done

--- UserFoodForGood/app/build.gradle.kts
+++ UserFoodForGood/app/build.gradle.kts
@@ -39,23 +39,38 @@ android {
         jvmTarget = "1.8"
     }
 }
-
 dependencies {
-
+    // Core Android KTX library
     implementation("androidx.core:core-ktx:1.12.0")
+    // Image slideshow library
     implementation("com.github.denzcoskun:ImageSlideshow:0.1.2")
+    // AndroidX AppCompat library
     implementation("androidx.appcompat:appcompat:1.6.1")
+    // Google Material Design Components library
     implementation("com.google.android.material:material:1.11.0")
+    // AndroidX ConstraintLayout library
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    // AndroidX Activity library
     implementation("androidx.activity:activity:1.8.0")
+    // AndroidX Navigation Fragment KTX library
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    // AndroidX Navigation UI KTX library
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    // Android Car UI library
     implementation("com.android.car.ui:car-ui-lib:2.6.0")
+    // Firebase Authentication library
     implementation("com.google.firebase:firebase-auth:23.0.0")
+    // Firebase Realtime Database library
     implementation("com.google.firebase:firebase-database:21.0.0")
+    // Firebase Storage library
     implementation("com.google.firebase:firebase-storage:21.0.0")
+    // JUnit testing framework
     testImplementation("junit:junit:4.13.2")
+    // AndroidX JUnit extension for Android tests
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    // AndroidX Espresso testing framework
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    // Google Sign-In library
+    implementation("com.google.android.gms:play-services-auth:20.5.0")
 }
--- UserFoodForGood/app/src/main/java/com/example/foodforgood/SignupActivity.kt
+++ UserFoodForGood/app/src/main/java/com/example/foodforgood/SignupActivity.kt
@@ -1,17 +1,23 @@
 package com.example.foodforgood
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.foodforgood.databinding.ActivitySignupBinding
 import com.example.foodforgood.model.UserModel
+import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.auth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DatabaseReference
@@ -24,7 +30,7 @@ class SignupActivity : AppCompatActivity() {
     private lateinit var userName: String
     private lateinit var auth: FirebaseAuth
     private lateinit var database: DatabaseReference
-    private lateinit var googleSignInAccount: GoogleSignInAccount
+    private lateinit var googleSignInClient: GoogleSignInClient
 
 
     private val binding: ActivitySignupBinding by lazy {
@@ -34,6 +40,8 @@ class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build()
 
         //Initializing firebase auth
         auth = Firebase.auth
@@ -41,7 +49,8 @@ class SignupActivity : AppCompatActivity() {
         //Initializing firebase database
         database = Firebase.database.reference
 
-
+        //Initializing google signin
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions)
         binding.createAccountButton.setOnClickListener {
             email = binding.emailAddress.text.toString().trim()
             password = binding.password.text.toString().trim()
@@ -60,32 +69,64 @@ class SignupActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-    }
-
-    private fun createAccount(email: String, password: String) {
-        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                Toast.makeText(this, "Account Created", Toast.LENGTH_SHORT).show()
-                saveUserData()
-                startActivity(Intent(this, LoginActivity::class.java))
-                finish()
-            } else {
-                Toast.makeText(this, "Failed to create account", Toast.LENGTH_SHORT).show()
-                Log.d("Account", "createUserWithEmail:failure", task.exception)
-            }
+        binding.googleSignIn.setOnClickListener{
+            val SingIntent = googleSignInClient.signInIntent
+            launcher.launch(SingIntent)
         }
     }
 
-    private fun saveUserData() {
-        //retrieve data from input field
-        userName = binding.userName.text.toString()
-        password = binding.password.text.toString().trim()
-        email = binding.emailAddress.text.toString().trim()
 
-        val user = UserModel(userName, email, password)
-        val userId = FirebaseAuth.getInstance().currentUser!!.uid
+    //launcher for google singin
+    private val launcher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {result->
+            if(result.resultCode == Activity.RESULT_OK){
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                if(task.isSuccessful){
+                    val account:GoogleSignInAccount? = task.result
+                    val credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+                    auth.signInWithCredential(credential).addOnCompleteListener{task->
+                        if(task.isSuccessful){
+                            startActivity(Intent(this, MainActivity::class.java))
+                            finish()
+                        }
+                        else{
+                            Toast.makeText(this, "Signin failed ${task.exception.toString()}", Toast.LENGTH_SHORT).show()
+                        }
 
-        //save data to firebasedatabase
-        database.child("user").child(userId).setValue(user)
-    }
-}
+                    }
+                }
+                else{
+                    Toast.makeText(this, "Signin failed ${task.exception.toString()}", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else{
+                Toast.makeText(this, "SignIn Failed", Toast.LENGTH_SHORT).show()
+            }
+        }
+            private fun createAccount(email: String, password: String) {
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "Account Created", Toast.LENGTH_SHORT).show()
+                        saveUserData()
+                        startActivity(Intent(this, LoginActivity::class.java))
+                        finish()
+                    } else {
+                        Toast.makeText(this, "Failed to create account", Toast.LENGTH_SHORT).show()
+                        Log.d("Account", "createUserWithEmail:failure", task.exception)
+                    }
+                }
+            }
+
+            private fun saveUserData() {
+                //retrieve data from input field
+                userName = binding.userName.text.toString()
+                password = binding.password.text.toString().trim()
+                email = binding.emailAddress.text.toString().trim()
+
+                val user = UserModel(userName, email, password)
+                val userId = FirebaseAuth.getInstance().currentUser!!.uid
+
+                //save data to firebasedatabase
+                database.child("user").child(userId).setValue(user)
+            }
+        }
