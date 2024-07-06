b97353227daec38344708068f8413773ee7de752
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-07-07 04:36:43 +0530

    Finailly app is working after so much efforts

--- AdminFoodForGood/app/src/main/java/com/example/adminfoodforgood/adapter/PendingOrderAdapter.kt
+++ AdminFoodForGood/app/src/main/java/com/example/adminfoodforgood/adapter/PendingOrderAdapter.kt
@@ -1,47 +1,62 @@
 package com.example.adminfoodforgood.adapter
 
 import android.content.Context
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.location.LocationRequestCompat.Quality
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.example.adminfoodforgood.databinding.PendingOrdersItemBinding
 
 class PendingOrderAdapter(
-    private val customerNames: ArrayList<String>,
-    private val quantities: ArrayList<String>,
-    private val foodImages: ArrayList<Int>,
-    private val context: Context
-): RecyclerView.Adapter<PendingOrderAdapter.pendingOrderViewHolder>() {
+    private val context: Context,
+    private val customerNames: MutableList<String>,
+    private val quantities: MutableList<String>,
+    private val foodImages: MutableList<String>
+) : RecyclerView.Adapter<PendingOrderAdapter.PendingOrderViewHolder>() {
 
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): pendingOrderViewHolder {
-        val binding = PendingOrdersItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
-        return pendingOrderViewHolder(binding)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PendingOrderViewHolder {
+        val binding =
+            PendingOrdersItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PendingOrderViewHolder(binding)
     }
 
-
-    override fun onBindViewHolder(holder: pendingOrderViewHolder, position: Int) {
-        holder.bind(position)
+    override fun onBindViewHolder(holder: PendingOrderViewHolder, position: Int) {
+        holder.bind(customerNames[position], quantities[position], foodImages[position])
     }
 
     override fun getItemCount(): Int = customerNames.size
 
-    inner class pendingOrderViewHolder(private val binding: PendingOrdersItemBinding):RecyclerView.ViewHolder(binding.root) {
-        private var isAccected = false
-        fun bind(position: Int) {
+    inner class PendingOrderViewHolder(private val binding: PendingOrdersItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        private var isAccepted = false
+
+        fun bind(foodName: String, foodPrice: String, foodImage: String) {
             binding.apply {
-                customerName.text = customerNames[position]
-                pendingOrderQuentity.text = quantities[position]
-                orderFoodItemImage.setImageResource(foodImages[position])
+                customerName.text = foodName
+                pendingOrderQuentity.text = foodPrice
+
+                // Log the image URL
+                Log.d("Adapter", "Image URL: $foodImage")
+
+                Glide.with(itemView.context)
+                    .load(foodImage)
+                    .override(300, 200) // Set the width and height to 300 and 200 respectively
+                    .into(orderFoodItemImage)
+
                 orderAcceptButton.setOnClickListener {
-                    if (!isAccected) {
+                    if (!isAccepted) {
                         orderAcceptButton.text = "Dispatch"
-                        isAccected = true
+                        isAccepted = true
                         showToast("Order Accepted")
-                    }
-                    else{
+                    } else {
                         customerNames.removeAt(adapterPosition)
                         notifyItemRemoved(adapterPosition)
                         showToast("Order Dispatched")
@@ -53,7 +68,6 @@ class PendingOrderAdapter(
 
 
     private fun showToast(s: String) {
-        Toast.makeText(context,s,Toast.LENGTH_SHORT).show()
+        Toast.makeText(context, s, Toast.LENGTH_SHORT).show()
     }
-
 }
--- AdminFoodForGood/app/src/main/java/com/example/adminfoodforgood/pendingOrderActivity.kt
+++ AdminFoodForGood/app/src/main/java/com/example/adminfoodforgood/pendingOrderActivity.kt
@@ -5,24 +5,73 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.adminfoodforgood.adapter.PendingOrderAdapter
 import com.example.adminfoodforgood.databinding.ActivityPendingOrderBinding
+import com.example.adminfoodforgood.model.OrderDetails
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class pendingOrderActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPendingOrderBinding
+    private var listOfName: MutableList<String> = mutableListOf()
+    private var listOfQuantity: MutableList<String> = mutableListOf()
+    private var listOfImageFirstFoodOrder:MutableList<String> = mutableListOf()
+    private var listOfOrderItems: MutableList<OrderDetails> = mutableListOf()
+    private lateinit var database: FirebaseDatabase
+    private lateinit var databaseOrderDetails: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPendingOrderBinding.inflate(layoutInflater)
         setContentView(binding.root)
+//Initialize of database
+        database = FirebaseDatabase.getInstance()
+        //Initialize of databaseReference
+        databaseOrderDetails = database.getReference("orderDetails")
+        getOrderDetails()
 
-        binding.backButton.setOnClickListener{
+        binding.backButton.setOnClickListener {
             finish()
         }
-        val OrderCustomerName = arrayListOf("John Doe", "Jane Smith", "Michael Johnson")
-        val quantity = arrayListOf("8", "6", "5")
-        val orderFoodImage = arrayListOf(R.drawable.menu1, R.drawable.menu2, R.drawable.menu3)
+    }
+
+    private fun getOrderDetails() {
+        //retrieve order details from firebase database
+        databaseOrderDetails.addListenerForSingleValueEvent(object :ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for(orderSnapShot in snapshot.children){
+                    val orderDetails = orderSnapShot.getValue(OrderDetails::class.java)
+                    orderDetails?.let{
+                        listOfOrderItems.add(it)
+
+                    }
+                }
+                addDataToListForRecyclerView()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
+    }
 
 
-        val adapter = PendingOrderAdapter(OrderCustomerName, quantity, orderFoodImage, context = this)
-        binding.pendingOrderRecyclerView.adapter = adapter
-        binding.pendingOrderRecyclerView.layoutManager = LinearLayoutManager(this)
+    private fun addDataToListForRecyclerView() {
+        for (i in 0 until listOfOrderItems.size - 1) {
+            listOfOrderItems[i].foodNames?.firstOrNull()?.let {
+                listOfName.add(it)
+                listOfOrderItems[i].foodQuantities?.firstOrNull()?.let {
+                    listOfQuantity.add(it.toString())
+                    listOfOrderItems[i].foodImages?.firstOrNull()?.let {
+                        listOfImageFirstFoodOrder.add(it)
+                    }
+
+                    binding.pendingOrderRecyclerView.layoutManager = LinearLayoutManager(this)
+                    val adapter = PendingOrderAdapter(this, listOfName, listOfQuantity, listOfImageFirstFoodOrder)
+                    binding.pendingOrderRecyclerView.adapter = adapter
+                }
+            }
+        }
     }
 }
