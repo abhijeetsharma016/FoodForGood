d55ab68c3dc8da5709a50df9182cd4de56094dd9
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-07-12 04:15:42 +0530

    set up number of pending orders and completed orders in main Activity

--- AdminFoodForGood/app/src/main/java/com/example/adminfoodforgood/MainActivity.kt
+++ AdminFoodForGood/app/src/main/java/com/example/adminfoodforgood/MainActivity.kt
@@ -4,12 +4,20 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.adminfoodforgood.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private lateinit var database: FirebaseDatabase
+    private lateinit var auth: FirebaseAuth
+    private lateinit var completedOrderReference: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
@@ -42,5 +50,42 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this,pendingOrderActivity::class.java)
             startActivity(intent)
         }
+
+        pendingOrders()
+
+        completedOrders()
     }
+
+    private fun pendingOrders() {
+        database = FirebaseDatabase.getInstance()
+        var pendingOrderRef = database.reference.child("orderDetails")
+        var pendingOrderItemCount = 0
+        pendingOrderRef.addListenerForSingleValueEvent(object :ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                pendingOrderItemCount = snapshot.childrenCount.toInt()
+                binding.pendingOrders.text = pendingOrderItemCount.toString()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+
+    }
+    private fun completedOrders() {
+        var completedOrderRef = database.reference.child("CompletedOrder")
+        var completedOrderItemCount = 0
+        completedOrderRef.addListenerForSingleValueEvent(object :ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                completedOrderItemCount = snapshot.childrenCount.toInt()
+                binding.completedOrders.text = completedOrderItemCount.toString()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+    }
+
+
 }
